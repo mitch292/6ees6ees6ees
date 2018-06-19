4b0c42eb95bf156104ebcf3b450f5d8709ba2c4c
Andrew Mitchell <andrewpmitchell7@gmail.com>
2018-06-19 12:02:28 -0400

    completed up to retiredForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage(treasure) {
+    var treasure = treasure || 'gold';
+    this.treasureChest.push(treasure);
+  }
 };
